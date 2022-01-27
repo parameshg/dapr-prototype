3a6e40c44ad4caa9930e186e5c62d517efe6269e
Paramesh Gunasekaran <paramesh.gunasekaran@gmail.com>
2022-01-27 16:32:25 +1100

    update: added secrets and configuration

--- README.md
+++ README.md
@@ -15,8 +15,8 @@ The following lists shows the completion status of protoype with Dapr features i
 - [ ] Resource Bindings and Triggers
 - [ ] Actors
 - [ ] Observability
-- [ ] Secrets
-- [ ] Configuration
+- [x] Secrets (Partially)
+- [x] Configuration (Partially)
 
 ##### Kubernetes
 
@@ -27,12 +27,14 @@ The following lists shows the completion status of protoype with Dapr features i
 - [ ] Resource Bindings and Triggers
 - [ ] Actors
 - [ ] Observability
-- [ ] Secrets
-- [ ] Configuration
-- [ ] Logging with ELK and Fluentd
-- [ ] Custom Component for IdentityServer
+- [x] Secrets (Partially)
+- [x] Configuration (Partially)
+- [ ] Funneling Dapr logs into ELK with Fluentd
+- [ ] Rate limiting and Bearer token using OIDC
 
 #### References
 
 - https://dapr.io
 - https://github.com/dapr
+- https://aka.ms/dapr-discord (Discord)
+- https://us02web.zoom.us/j/85305980190 (Community Meeting, Password: eWRhSklVTjJjSnhTaURDcFZaU2ZzQT09)
--- frontend/Controllers/HomeController.cs
+++ frontend/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using Dapr.Client;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
 using System.Text.Json.Serialization;
@@ -11,6 +13,10 @@ namespace Frontend.Controllers
     [ApiController]
     public class HomeController : ControllerBase
     {
+        private const string CONFIGURATION = "map";
+
+        private const string SECRET = "vault";
+
         private DaprClient Dapr { get; set; }
 
         public HomeController()
@@ -34,10 +40,65 @@ namespace Frontend.Controllers
             return await Dapr.CheckHealthAsync();
         }
 
+        [HttpGet("/config/{key}")]
+        public async Task<string> GetConfiguration(string key, [FromQuery] bool secret)
+        {
+            var result = string.Empty;
+
+            if (secret)
+            {
+                result = (await Dapr.GetSecretAsync(SECRET, key))?.FirstOrDefault().Value;
+            }
+            else
+            {
+                //TODO: This is obsolete and not stable
+
+                var configuration = await Dapr.GetConfiguration(CONFIGURATION, new List<string>() { key });
+
+                if (configuration != null && configuration.Items != null && configuration.Items.Count.Equals(1))
+                {
+                    result = configuration.Items[0].Value;
+                }
+            }
+
+            return result;
+        }
+
+        [HttpPost("/config/{key}")]
+        public async Task<bool> SetConfiguration(string key, [FromBody] string value, [FromQuery] bool secret)
+        {
+            var result = false;
+
+            if (secret)
+            {
+                //TODO: This is not yet available
+            }
+            else
+            {
+                //TODO: This is not yet available
+
+                //var configuration = await Dapr.SetConfiguration(CONFIGURATION, new List<string>() { key });
+
+                //if (configuration != null && configuration.Items != null && configuration.Items.Count.Equals(1))
+                //{
+                //    result = configuration.Items[0].Value;
+                //}
+            }
+
+            return result;
+        }
+
         [HttpGet("/series")]
-        public async Task<dynamic> Series([FromQuery] int count = 10)
+        public async Task<dynamic> GetSeries([FromQuery] int count = 10)
         {
             dynamic result = new { count = 0, sum = 0, mean = 0, variance = 0, deviation = 0 };
+
+            var configuration = await Dapr.GetConfiguration(CONFIGURATION, new List<string>() { "defaults.series.count" });
+
+            if (configuration != null && configuration.Items != null && configuration.Items.Count.Equals(1))
+            {
+                count = int.Parse(configuration.Items[0].Value);
+            }
 
             async Task Fibonacci(int firstNumber, int secondNumber, int counter, int length)
             {
